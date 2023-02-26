e94b27fdece44cb40ff72ccfa9b3ec26c69d3751
MihailLeontev <mihailleontev177@gmail.com>
2023-02-26 12:17:24 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,11 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-
-// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-// rendered items
 function createGalleryItemsMarkup(items) {
   return items.map(({ preview, original, description }) => {
     return `
@@ -23,8 +19,6 @@ function createGalleryItemsMarkup(items) {
 `
   }).join('');
 }
-
-// use library SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
